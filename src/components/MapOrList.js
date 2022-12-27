0855c8b3052208e7e6d2b400036121694f973dea
sametckilic <84988439+sametckilic@users.noreply.github.com>
2022-12-27 14:50:33 +0300

    update

--- src/components/MapOrList.js
+++ src/components/MapOrList.js
@@ -3,54 +3,25 @@ import {useState} from 'react';
 
 export default function MapOrList({setMapOrList, mapOrList}) {
   
-  const [style, setStyle] = useState({backgroundColor:"#00B4DB", color:'white'})
-  const [_style, _setStyle] = useState({backgroundColor:"", color:'white'})
-
-
-  const onMouseOver = () =>{
-    
-    setStyle({backgroundColor:"white", color:"#153652"});
-  }
-  
-  const _onMouseOver = () =>{
-    
-    _setStyle({color:"#153652",backgroundColor:"white"});
-  }
-
-  const onMouseLeave = () =>{
-    if(mapOrList){
-      setStyle({backgroundColor:"#00B4DB", color:'white'})
-      _setStyle({backgroundColor:"", color:'white'})
-    }
-    else{
-      setStyle({backgroundColor:"", color:'white'})
-      _setStyle({backgroundColor:"#00B4DB", color:'white'})
-    }
-  }
+  const [style, setStyle] = useState({backgroundColor:"#051c30"})
 
   const clickMap = () =>{
     setMapOrList(true);
     console.log(`harita`);
-
   }
-  
   const clickList = () =>{
     setMapOrList(false);
     console.log(`liste`);
   }
-
   return (
     <div className = "mol-border">
-      <a className = "mol-item" style ={style}
-       onMouseEnter = {onMouseOver} 
-       onMouseLeave = {onMouseLeave}
+      <a className = "mol-item" 
+       style ={mapOrList?style:null}
        onClick = {()=>clickMap()}>
        Harita</a>
 
       <a className = "mol-item" 
-      style ={_style} 
-      onMouseEnter = {_onMouseOver}
-      onMouseLeave= {onMouseLeave}
+      style ={!mapOrList?style:null} 
       onClick = {()=>clickList()}>
       Liste</a>
     </div>
